ba2194b92623f1c8fadfc04a1a9390e9537efb7a
Tyler Trotter <tylertrotter@gmail.com>
2016-03-13 09:49:57 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    var height = $(window).height();
+    $('body > section:last-of-type').css('min-height', height);
+});
+
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
+$(window).scroll(function(){
+    var scrollTop = $(document).scrollTop();
+    $('#intro .wrapper').css('padding-top', scrollTop/2);
+    console.log(scrollTop);
+    if( scrollTop > $(window).height() - 150){
+        $('#main-nav').addClass('back-to-top-active');
+    }else{
+        $('#main-nav').removeClass('back-to-top-active');
+    }
+})
